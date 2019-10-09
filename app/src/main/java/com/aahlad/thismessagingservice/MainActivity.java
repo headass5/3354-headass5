1e4682c7ce6fbaa28e808262a88180821c9ca002
Al Madireddy <al.madireddy@gmail.com>
2019-10-09 18:18:45 -0500

    checking against password and redirecting if pref exists

--- app/src/main/java/com/aahlad/thismessagingservice/MainActivity.java
+++ app/src/main/java/com/aahlad/thismessagingservice/MainActivity.java
@@ -1,11 +1,14 @@
 package com.aahlad.thismessagingservice;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
   Button loginButton;
@@ -22,17 +25,38 @@ public class MainActivity extends AppCompatActivity {
     emailField = findViewById(R.id.login_email_field);
     passwordField = findViewById(R.id.login_password_field);
   
-    loginButton.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        Toast.makeText(getApplicationContext(), "Button clicked", Toast.LENGTH_LONG).show();
-        String enteredEmail = emailField.getText().toString();
-        String enteredPassword = passwordField.getText().toString();
-        
-        System.out.println(enteredEmail);
-        System.out.println(enteredPassword);
-      }
-    });
+    final SharedPreferences prefs = getSharedPreferences("Auth", Activity.MODE_PRIVATE);
+    boolean savedEmail = prefs.contains("login_email");
+    boolean savedPassword = prefs.contains("login_password");
+    final Intent sendToConversations = new Intent(MainActivity.this, Conversations.class);
+  
+    if (savedEmail && savedPassword && prefs.getString("login_password", "").equals("hiThere")) {
+      // Auth info exists: redirect to conversations
+      System.out.println("Redirecting");
+      startActivity(sendToConversations);
+    } else {
+      loginButton.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+          Toast.makeText(getApplicationContext(), "Button clicked", Toast.LENGTH_LONG).show();
+          String enteredEmail = emailField.getText().toString();
+          String enteredPassword = passwordField.getText().toString();
+      
+          // TODO: verify with Firebase auth
+          // Add login details to Shared Prefs
+          SharedPreferences.Editor editor = prefs.edit();
+          editor.putString("login_email", enteredEmail);
+          editor.putString("login_password", enteredPassword);
+          boolean worked = editor.commit();
+          
+          if (enteredPassword.equals("hiThere")) {
+            startActivity(sendToConversations);
+          }
+          System.out.println(worked);
+        }
+      });
+    }
+    
   }
   
 }
