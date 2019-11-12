4bb2e109c610c43156bdeb722ded8e81a96a3160
Pallavi <pxv170130@utdallas.edu>
2019-11-11 18:01:44 -0600

    Set up create contact page

--- app/src/main/java/com/aahlad/thismessagingservice/AddContact.java
+++ app/src/main/java/com/aahlad/thismessagingservice/AddContact.java
@@ -3,12 +3,30 @@ package com.aahlad.thismessagingservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class AddContact extends AppCompatActivity {
+
+  Button createContact;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_add_contact);
+
+    createContact = findViewById(R.id.create_contact);
+    createContact.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View view) {
+        System.out.println("Create Contact");
+
+      }
+    });
+
   }
 }
