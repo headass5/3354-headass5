e66a589ac33f46c06d973171f6640a2d081cacc7
Patrick Fellows <pfellows10014@gmail.com>
2019-11-18 09:13:18 -0600

    Branch created to add create conversation activity

--- app/src/main/java/com/aahlad/thismessagingservice/CreateConvoActivity.java
+++ app/src/main/java/com/aahlad/thismessagingservice/CreateConvoActivity.java
@@ -1,4 +1,58 @@
 package com.aahlad.thismessagingservice;
 
-public class CreateConvoActivity {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+public class CreateConvoActivity extends AppCompatActivity {
+    Button sendButton;
+    TextView emailField;
+    TextView convoBody;
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+
+    /*private boolean checkIfUserExist(FirebaseFirestore text){
+        auth.updateCurrentUser()
+    }*/
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_convo);
+
+        final Intent sendToMessage = new Intent(getBaseContext(), Conversations.class);
+
+        sendButton = findViewById(R.id.send_button);
+        emailField = findViewById(R.id.recipients_email_field);
+        convoBody = findViewById(R.id.convo_body);
+
+        sendButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String enteredEmail = emailField.getText().toString();
+                String enteredText = convoBody.getText().toString();
+
+                if (!enteredEmail.equals("")) {
+                    //checkIfUserExist(enteredEmail);
+                    startActivity(sendToMessage);
+                    finish();
+                } else {
+                }
+            }
+        });
+    }
 }
