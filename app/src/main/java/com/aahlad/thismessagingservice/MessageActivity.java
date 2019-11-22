31119daa4ee56ca9b74b999d2e49282538c94fad
Bishesh Manandhar <bishesh.manandhar@gmail.com>
2019-11-21 22:03:50 -0600

    messages are now sent into firestore database

--- app/src/main/java/com/aahlad/thismessagingservice/MessageActivity.java
+++ app/src/main/java/com/aahlad/thismessagingservice/MessageActivity.java
@@ -19,6 +19,7 @@ import com.aahlad.thismessagingservice.Model.Chat;
 import com.aahlad.thismessagingservice.Model.User;
 import com.bumptech.glide.Glide;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Tasks;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -26,12 +27,18 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
@@ -39,7 +46,6 @@ public class MessageActivity extends AppCompatActivity {
     CircleImageView profile_image;
     TextView username;
     FirebaseUser currentUser;
-    FirebaseFirestore db;
 
     ImageButton btn_send;
     EditText text_send;
@@ -51,9 +57,17 @@ public class MessageActivity extends AppCompatActivity {
 
     Intent intent;
 
-
     // Refactor
     DatabaseReference reference;
+
+    Date date = new Date();
+
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private FirebaseAuth auth = FirebaseAuth.getInstance();
+
+    public Date getTimestamp(){
+        return date;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -88,6 +102,15 @@ public class MessageActivity extends AppCompatActivity {
 
         currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
+        db.collection(Constants.USER_META_PATH).document(userid).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                User user = documentSnapshot.toObject(User.class);
+                assert user != null;
+                username.setText(user.getUsername());
+            }
+        });
+
         // Refactor
         btn_send.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -101,36 +124,83 @@ public class MessageActivity extends AppCompatActivity {
                 text_send.setText("");
             }
         });
-        
-        //currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        db = FirebaseFirestore.getInstance();
-        db.collection(Constants.USER_META_PATH).document(currentUser.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-          @Override
-          public void onSuccess(DocumentSnapshot documentSnapshot) {
-            User user = documentSnapshot.toObject(User.class);
-            assert user != null;
-            
-            username.setText(user.getUsername());
-            if (user.getImageURL().equals("default")) {
-              profile_image.setImageResource(R.mipmap.ic_launcher);
-            } else {
-              Glide.with(MessageActivity.this).load(user.getImageURL()).into(profile_image);
-            }
-              readMessages(currentUser.getUid(), userid, user.getImageURL());
-          }
-        });
 
+        db.collection(Constants.USER_META_PATH).document(currentUser.getUid()).get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+            @Override
+            public void onSuccess(DocumentSnapshot documentSnapshot) {
+                User user = documentSnapshot.toObject(User.class);
+                assert user != null;
+
+                //username.setText(user.getUsername());
+                if (user.getImageURL().equals("default")) {
+                    profile_image.setImageResource(R.mipmap.ic_launcher);
+                } else {
+                    Glide.with(MessageActivity.this).load(user.getImageURL()).into(profile_image);
+                }
+                readMessages(currentUser.getUid(), userid, user.getImageURL());
+            }
+        });
     }
 
-    private void sendMessage(String sender, String receiver, String message) {
-        DatabaseReference reference = FirebaseDatabase.getInstance().getReference();
+    private void sendMessage(String sender, final String receiver, final String message) {
+        Thread addConvo = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try{
+                    String currentUserId = auth.getCurrentUser().getUid();
+                    String currentUserName = auth.getCurrentUser().getDisplayName();
 
-        HashMap<String, Object>hashMap = new HashMap<>();
-        hashMap.put("sender", sender);
-        hashMap.put("receiver", receiver);
-        hashMap.put("message", message);
+                    QuerySnapshot otherUser = Tasks.await(db.collection(Constants.USER_META_PATH)
+                            .whereEqualTo("id", receiver)
+                            .get());
 
-        reference.child("Chats").push().setValue(hashMap);
+                    if (otherUser.isEmpty()) {
+                        System.out.println("Here is the username");
+                        System.out.println(username);
+                        finish();
+                        return;
+                    }
+                    DocumentSnapshot otherDocument = otherUser.getDocuments().get(0);
+
+                    List<String> usernames = new ArrayList<>();
+                    usernames.add(currentUserName);
+                    usernames.add(otherDocument.get("username").toString());
+                    Collections.sort(usernames);
+
+                    StringBuilder docIDBuilder = new StringBuilder();
+                    for(int i = 0; i < usernames.size(); i++){
+                        docIDBuilder.append(usernames.get(i));
+                    }
+
+                    String docID = docIDBuilder.toString();
+
+                    Map<String, Object> convoData = new HashMap<>();
+                    List<String> users = new ArrayList<>();
+                    users.add(currentUserId);
+                    users.add(otherDocument.getId());
+                    Collections.sort(users);
+
+                    convoData.put("title", currentUserName + " & " + otherDocument.get("username"));
+                    convoData.put("users", users);
+
+                    Tasks.await(db.collection(Constants.CONVERSATIONS_PATH).document(docID.toString()).set(convoData));
+
+                    Map<String, Object> messageData = new HashMap<>();
+
+                    messageData.put("body", message);
+                    messageData.put("convoID", docID.toString());
+                    messageData.put("time_stamp", getTimestamp());
+                    messageData.put("userID", currentUserId);
+
+                    Tasks.await(db.collection(Constants.MESSAGES_PATH).add(messageData));
+                    //finish();
+
+                }catch (InterruptedException | ExecutionException e) {
+                    System.out.println("Create Contact exception");
+                }
+            }
+        });
+        addConvo.start();
     }
 
     private void readMessages(final String myid, final String userid, final String imageurl) {
@@ -144,7 +214,7 @@ public class MessageActivity extends AppCompatActivity {
                 for (DataSnapshot snapshot: dataSnapshot.getChildren()) {
                     Chat chat = snapshot.getValue(Chat.class);
                     if(chat.getReceiver().equals(myid) && chat.getSender().equals(userid) ||
-                        chat.getReceiver().equals(userid) && chat.getSender().equals(myid)) {
+                            chat.getReceiver().equals(userid) && chat.getSender().equals(myid)) {
                         mchat.add(chat);
                     }
 
